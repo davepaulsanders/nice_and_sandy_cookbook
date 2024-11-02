9604b8bfd5eb67e528fad264c5f7adcbded6d7a9
= <=>
2024-11-02 16:04:46 -0400

    Using children instead of Children.toArray

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,11 +1,11 @@
-import { Children, ReactElement } from "react"
+import { ReactElement } from "react"
 import Wrapper from "../Wrapper/Wrapper"
 const Category = ({category, children}: {category: string, children: ReactElement[]}) => {
 	return (
 		<section className="my-16">
 		<p className="font-bold text-4xl my-10 text-center">{category}</p>
 		<Wrapper>
-		{Children.toArray(children) as ReactElement[]}
+		{children as ReactElement[]}
 		</Wrapper>
 		</section>
 	)
