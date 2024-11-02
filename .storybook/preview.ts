db9487428b289cb0ffded3a60452caac1ddd4c30
= <=>
2024-11-02 11:08:42 -0400

    Adding viewport addon to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,12 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css"
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 const preview: Preview = {
   parameters: {
+	viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
     actions: { argTypesRegex: '^on.*' },
     controls: {
       matchers: {
