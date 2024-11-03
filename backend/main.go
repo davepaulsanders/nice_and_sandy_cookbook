84bd6040004ecf607ff1f5ab54a326e84f7498bd
David Sanders <davepaulsanders@gmail.com>
2024-11-03 14:33:14 -0500

    Initial go server with sqlite set up

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "net/http"
+  "database/sql"
+  "github.com/gin-gonic/gin"
+   _ "github.com/mattn/go-sqlite3"
+  "log"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./recipes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+  r := gin.Default()
+  r.GET("/healthz", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "healthy",
+    })
+  })
+  r.GET("/recipes", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "healthy",
+    })
+  })
+  r.Run()
+}
