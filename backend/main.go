95b2ba953bbb1cd2348bdbd12cf06163e1c3fa33
= <=>
2024-11-16 10:41:37 -0500

    Creating RouteHandler struct and methods

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
         "github.com/gin-gonic/gin"
         _ "github.com/mattn/go-sqlite3"
         "log"
-        "net/http"
 )
 
 func main() {
@@ -18,95 +17,20 @@ func main() {
                 log.Fatal(err)
         }
         defer db.Close()
+
         r := gin.Default()
+
+		handler := &RouteHandler{} 
+
+		if err = handler.setUp(db); err != nil {
+			log.Fatalf("Error setting up RouteHandler")
+		}
+
 		v1 := r.Group("/v1") 
-		
-        v1.GET("/healthz", func(c *gin.Context) {
-                c.JSON(http.StatusOK, gin.H{
-                        "message": "healthy",
-                })
-        })
-		v1.GET("/favicon.ico", func(c *gin.Context) {
-			    c.Status(http.StatusNoContent)  
-		})
-		v1.GET("/categories", func (c *gin.Context) {
-                type Category struct {
-                        Id         int            `json:"id"`
-                        Category   string         `json:"category"`
-                }
-                var categories []Category
-                resp, err := db.Query("SELECT * FROM categories;")
-                if err != nil {
-                        c.JSON(http.StatusInternalServerError, err)
-                }
-                defer resp.Close()
-                for resp.Next() {
-                        var category Category 
-                        if err := resp.Scan(&category.Id, &category.Category); err != nil {
-                                log.Fatal(err)
-                        }
-                        categories = append(categories, category)
-                }
-                c.JSON(http.StatusOK, gin.H{
-                        "categories": categories,
-                })
-
-		})
-        v1.GET("/recipes", func(c *gin.Context) {
-
-                type Recipe struct {
-                        Id         int            `json:"id"`
-                        Img        sql.NullString `json:"img"`
-                        Href       sql.NullString `json:"href"`
-                        Label      string         `json:"label"`
-                        Alt        string         `json:"alt"`
-                        CategoryId int            `json:"category_id"`
-                        IsPinned   bool           `json:"is_pinned"`
-                }
-                var recipes []Recipe
-                resp, err := db.Query("SELECT * FROM recipes;")
-                if err != nil {
-                        c.JSON(http.StatusInternalServerError, err)
-                }
-                defer resp.Close()
-                for resp.Next() {
-                        var recipe Recipe
-                        if err := resp.Scan(&recipe.Id, &recipe.Img, &recipe.Href, &recipe.Label, &recipe.Alt, &recipe.CategoryId, &recipe.IsPinned); err != nil {
-                                log.Fatal(err)
-                        }
-                        recipes = append(recipes, recipe)
-                }
-                c.JSON(http.StatusOK, gin.H{
-                        "recipes": recipes,
-                })
-        })
-        v1.GET("/recipes/pinned", func(c *gin.Context) {
-
-                type Recipe struct {
-                        Id         int            `json:"id"`
-                        Img        sql.NullString `json:"img"`
-                        Href       sql.NullString `json:"href"`
-                        Label      string         `json:"label"`
-                        Alt        string         `json:"alt"`
-                        CategoryId int            `json:"category_id"`
-                        IsPinned   bool           `json:"is_pinned"`
-                }
-                var recipes []Recipe
-                resp, err := db.Query("SELECT * FROM recipes WHERE is_pinned = true;")
-                if err != nil {
-                        c.JSON(http.StatusInternalServerError, err)
-                }
-                defer resp.Close()
-                for resp.Next() {
-                        var recipe Recipe
-                        if err := resp.Scan(&recipe.Id, &recipe.Img, &recipe.Href, &recipe.Label, &recipe.Alt, &recipe.CategoryId, &recipe.IsPinned); err != nil {
-                                log.Fatal(err)
-                        }
-                        recipes = append(recipes, recipe)
-                }
-                c.JSON(http.StatusOK, gin.H{
-                        "recipes": recipes,
-                })
-        })
+        v1.GET("/healthz", handler.healthCheck)
+		v1.GET("/favicon.ico", handler.faviconNoContent)
+		v1.GET("/categories", handler.categories) 
+        v1.GET("/recipes", handler.recipes)
+        v1.GET("/recipes/pinned", handler.pinnedRecipes)
         r.Run()
 }
