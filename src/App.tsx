986fc933dd49c9570f3a8e41696e7a0f6bcccf60
David Sanders <davepaulsanders@gmail.com>
2024-11-17 07:07:12 -0500

    Rendering recipes for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState, useEffect, Fragment } from "react"
 import './App.css'
+import { fetchData } from "./utils/fetch"
+import Home from "./pages/Home/Home"
+import Category from "./components/Category/Category"
+import RecipeCard from "./components/RecipeCard/RecipeCard"
 
-function App() {
+const App = () => {
+const [recipes, setRecipes] = useState<Map<string, any[]>>()
+ useEffect(() => {
+	getRecipes()
+}, [])
+const getRecipes = async () => {
+	const { recipes: recipeList } = await fetchData("http://localhost:8080/v1/recipes")
+	const recipeMap = new Map()
+	recipeList.forEach(recipe => {
+		if (!recipeMap.get(recipe.category)) {
+			recipeMap.set(recipe.category, [<RecipeCard img={recipe.img} href={recipe.href} label={recipe.label} alt={recipe.alt}/>])	
+		} else {
+			recipeMap.get(recipe.category)!.push(<RecipeCard img={recipe.img} href={recipe.href} label={recipe.label} alt={recipe.alt}/>)
+		}
+	})
+	setRecipes(recipeMap)
 
+}
   return (
-    <div>
-	<p className='text-4xl'>Hey</p>
-    </div>
+	  <>
+	<Home />
+	{recipes && Array.from(recipes).map(([key, val]) => {
+		return (<Category category={key} children={val} />)
+	})}
+	</>
   )
 }
 
