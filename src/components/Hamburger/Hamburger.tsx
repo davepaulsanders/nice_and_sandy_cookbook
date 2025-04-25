f3698a688238c4dcf144a2e3fa5778aca77a65f9
David Sanders <davepaulsanders@gmail.com>
2025-04-25 19:38:17 -0400

    Adding HamburgerMenu and toggleOpen hook

---
+++ src/components/Hamburger/Hamburger.tsx
@@ -0,0 +1,18 @@
+import useToggle from "../../hooks/useToggle"
+const HamburgerMenu = () => {
+	const { isOpen, toggleOpen } = useToggle()
+	return (
+		<button onClick={() => toggleOpen(prev => !prev)}
+			  className="flex flex-col justify-center items-center w-[20px] h-[20px] absolute top-5 right-10">
+				<span className={`bg-lightg block transition-all duration-300 ease-out 
+								h-0.5 w-6 rounded-sm ${isOpen ? 
+								'rotate-45 translate-y-1' : '-translate-y-0.5'
+								}`} >
+					</span>
+					<span className={`bg-lightg block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}> </span>
+				<span className={`bg-lightg block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}> </span>
+
+		  </button>
+	)
+}
+export default HamburgerMenu
