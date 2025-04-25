e24e470d152fafca3c660aeb4988ffcb637264bb
David Sanders <davepaulsanders@gmail.com>
2025-04-25 19:40:32 -0400

    Setting to fixed position

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -3,7 +3,7 @@ const HamburgerMenu = () => {
 	const { isOpen, toggleOpen } = useToggle()
 	return (
 		<button onClick={() => toggleOpen(prev => !prev)}
-			  className="flex flex-col justify-center items-center w-[20px] h-[20px] absolute top-5 right-10">
+			  className="fixed flex flex-col justify-center items-center w-[20px] h-[20px] top-5 right-10">
 				<span className={`bg-lightg block transition-all duration-300 ease-out 
 								h-0.5 w-6 rounded-sm ${isOpen ? 
 								'rotate-45 translate-y-1' : '-translate-y-0.5'
