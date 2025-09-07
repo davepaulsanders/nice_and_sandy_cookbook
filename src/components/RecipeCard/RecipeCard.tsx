40c6698f79bd55c9d3d400257655e71706502a14
David Sanders <davepaulsanders@gmail.com>
2025-09-07 13:00:39 -0400

    Fixing target blank to _blank and adding noopener noreferrer

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -5,29 +5,29 @@ import cancelPin from "/thumb-tack-cancel.webp";
 import { pinRecipeToggle } from "./RecipeCardUtils";
 import { Recipe } from "../../types/types";
 interface RecipeCardProps extends Recipe {
-	recipes: Recipe[];
-	setRecipes: React.Dispatch<React.SetStateAction<Recipe[]>>;
-	setRecipeSearchCopy?: React.Dispatch<React.SetStateAction<Recipe[]>>;
+  recipes: Recipe[];
+  setRecipes: React.Dispatch<React.SetStateAction<Recipe[]>>;
+  setRecipeSearchCopy?: React.Dispatch<React.SetStateAction<Recipe[]>>;
 }
 const RecipeCard = (props: RecipeCardProps) => {
-	return (
-		<a href={props.href} target="blank">
-			<div
-				className="cursor-pointer relative flex flex-col justify-content-center \
+  return (
+    <a href={props.href} target="_blank" rel="noopener noreferrer">
+      <div
+        className="cursor-pointer relative flex flex-col justify-content-center \
 		items-center "
-			>
-				<CardHeader {...props} />
-				<CardContent {...props} />
-				<img
-					data-testid="pin"
-					onClick={(e) => pinRecipeToggle(e, props)}
-					width="30px"
-					height="30px"
-					className="absolute top-5 right-1 hover:scale-125 hover:cursor-pointer"
-					src={props.is_pinned ? cancelPin : tack}
-				/>
-			</div>
-		</a>
-	);
+      >
+        <CardHeader {...props} />
+        <CardContent {...props} />
+        <img
+          data-testid="pin"
+          onClick={(e) => pinRecipeToggle(e, props)}
+          width="30px"
+          height="30px"
+          className="absolute top-5 right-1 hover:scale-125 hover:cursor-pointer"
+          src={props.is_pinned ? cancelPin : tack}
+        />
+      </div>
+    </a>
+  );
 };
 export default RecipeCard;
