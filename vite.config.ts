72c12c44cf107204e817623f90f6e4e12a2f1a62
David Sanders <davepaulsanders@gmail.com>
2025-11-08 10:01:44 -0500

    Updating vite config for ts issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-server: {
-proxy: {
-    '/v1': {
-      target: 'http://go-recipes:8080',
-      changeOrigin: true,
-    }
-  }
-},	
+export default defineConfig(() => ({
+  server: {
+    proxy: {
+      "/v1": {
+        target: "http://go-recipes:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
   test: {
-   environment: 'happy-dom',
-   setupFiles: './testSetUp.ts'
-}
-})
+    environment: "happy-dom",
+    setupFiles: "./testSetUp.ts",
+  },
+}));
